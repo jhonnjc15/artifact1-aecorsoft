65dfb1170d7bdc56f67256630347da21280f39d7
jhonnjc15 <jhonatanjcca@gmail.com>
2026-06-22 13:13:03 -0500

    Nueva prueba athena create

--- src/sql/create_table_aecorsoft.sql
+++ src/sql/create_table_aecorsoft.sql
@@ -2,13 +2,18 @@ CREATE EXTERNAL TABLE IF NOT EXISTS db_aecorsoft_dev.aecorsoft_data (
   kostl STRING,
   ktext STRING
 )
-PARTITIONED BY (codproceso STRING)
-ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
-STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
-OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
-LOCATION 's3://ue1stgtestas3dtl001-landing/UE1STGTESTS3LOG001/SAP/CSKT/prueba/'
+PARTITIONED BY (
+  codproceso STRING
+)
+ROW FORMAT SERDE
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://ue1stgtestas3dtl001-landing/UE1STGTESTS3LOG001/SAP/CSKT/prueba/'
 TBLPROPERTIES (
-  'classification' = 'parquet',
+  'classification' = 'parquet'
   'table_type' = 'EXTERNAL_TABLE',
-  'parquet.compress' = 'SNAPPY'
 );
