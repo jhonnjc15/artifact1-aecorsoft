640b13058ba5dec9c7f9c95af6468884de1a95db
jhonnjc15 <jhonatanjcca@gmail.com>
2026-06-22 11:56:45 -0500

    Prueba de creación de tabla athena

--- src/sql/create_table_aecorsoft.sql
+++ src/sql/create_table_aecorsoft.sql
@@ -1,11 +1,12 @@
 CREATE EXTERNAL TABLE IF NOT EXISTS db_aecorsoft_dev.aecorsoft_data (
-  ingestion_ts TIMESTAMP
+  kostl STRING,
+  ktext STRING
 )
 PARTITIONED BY (codproceso STRING)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
 STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
-LOCATION 's3://artifact1-aecorsoft-landing-850995559699-us-east-1/AECORSOFT/aecorsoft_data/'
+LOCATION 's3://ue1stgtestas3dtl001-landing/UE1STGTESTS3LOG001/SAP/CSKT/prueba/'
 TBLPROPERTIES (
   'classification' = 'parquet',
   'table_type' = 'EXTERNAL_TABLE',
